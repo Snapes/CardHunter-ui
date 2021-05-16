ecc1d62170d61f9177d85c8f2d67cf2fb37b404a
Snapes <georgesnapes95@gmail.com>
2021-05-16 02:03:21 +0000

    snapes: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import {Container, TextField, Typography, Box} from '@material-ui/core';
+import { Autocomplete} from '@material-ui/lab';
+// import { DataGrid } from '@material-ui/data-grid';
+import MUIDataTable from "mui-datatables";
+import axios from "axios"
+import { GridNoRowsOverlay } from '@material-ui/data-grid';
+
+const columns = [
+    "Card",
+    "Hobbymaster",
+    "Hareruya EN",
+    "Hareruya JP",
+    "Baydragon",
+    "Goblin Games",
+    "Spellbound",
+    "Magic at Willis",
+    "Iron Knight Gaming",
+    "Magic Magpie"
+];
+
+const rows = [
+  ['Fervor', 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+  ['Counterspell', 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+];
+
+const options = {
+  filterType: 'checkbox',
+};
+
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+]
+
+
+class AppTable extends React.Component {
+
+  render() {
+
+    const [columns, setColumns] = useState([])
+    useEffect(() => {
+      axios.get('http://127.0.0.1:5000/parsers').then(res => setColumns(res.data))
+      .catch(err => console.log(err.message))
+    }, [])
+  
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      axios.post('http://127.0.0.1:5000/', {
+        card: 'Fervor',
+      }).then(res => setData(res.data))
+      .catch(err => console.log(err.message))
+    }, [])
+
+    return (
+      <Container maxWidth="xl">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            MTG Card Hunter
+          </Typography>
+          <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          options={top100Films.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+          )}
+        />
+          <MUIDataTable title={"Prices"} data={data} columns={columns} options={options}/>
+        </Box>
+      </Container>
+    );
+  }
+}
+
+export default AppTable
